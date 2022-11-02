e340a5aa9230566d82c85298df2f5158a8e60a54
prashant6311 <prashantppatil761@gmail.com>
2022-11-02 10:13:30 +0530

    Add multiple person in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -10,24 +10,31 @@ public class AddressBookMain {
 		System.out.println("Welcome to master branch ");
 
 		AddressBookMethod addressBookMethod = new AddressBookMethod();
-		addressBookMethod.newContact();
-		addressBookMethod.newContact();
+		boolean factos = true;
 
-		System.out.println("if you want to edit any contact press 1");
-		System.out.println("if you want to delete any contact press 2");
-		int check = scanner.nextInt();
+		while (factos == true) {
 
-		if (check == 1) {
-			addressBookMethod.editContact();
+			System.out.println("if you want to add new contact press 1");
+			System.out.println("if you want to edit any contact press 2");
+			System.out.println("if you want to delete any contact press 3");
+			System.out.println("if you want to display contact press 4");
+			System.out.println("if you want to exit press 5");
+			byte check = scanner.nextByte();
 
-		} else if (check == 2) {
-			addressBookMethod.deleteContact();
-
-		} else {
-
-			System.out.println("Thanks for using this Address-book");
+			if (check == 1) {
+				addressBookMethod.newContact();
+			} else if (check == 2) {
+				addressBookMethod.editContact();
+			} else if (check == 3) {
+				addressBookMethod.deleteContact();
+			} else if (check == 4) {
+				System.out.println(addressBookMethod.list.toString());
+			} else if (check == 5) {
+				System.out.println("Thanks for using this Address-book");
+				factos = false;
+			} else {
+				System.out.println("Please choose correct option");
+			}
 		}
-		System.out.println(addressBookMethod.list);
-
 	}
 }
